cfdc7fa2cc92a90702cebc185f83970b77f0b44a
akshay khale <akshay.khale@vamaship.com>
2017-09-09 20:13:13 +0530

    Updated App-homepage

--- app-homepage/process/typescript/boot.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from 'angular2/core';
-import {enableProdMode} from 'angular2/core';//Uncomment to enable production mode
-import {bootstrap} from 'angular2/platform/browser';
-import {AdvancedComponent} from './advanced_component.component';
-
-// enableProdMode();//Uncomment to enable production mode
-bootstrap(AdvancedComponent);
