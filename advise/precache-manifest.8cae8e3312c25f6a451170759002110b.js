10280654fad3d317f855fd6b31ca0cc2cc7401bd
Akshay Khale <khaleakshay@gmail.com>
2020-07-07 00:51:30 +0530

    Added simple React APP

---
+++ advise/precache-manifest.8cae8e3312c25f6a451170759002110b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6da879953731672c3ce6a791bcc0be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2946b68346efa146fa0a",
+    "url": "/static/css/main.477b6c71.chunk.css"
+  },
+  {
+    "revision": "66083c744488738df489",
+    "url": "/static/js/2.ff37d649.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff37d649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2946b68346efa146fa0a",
+    "url": "/static/js/main.ae9bff55.chunk.js"
+  },
+  {
+    "revision": "573c1ef5acecc4a28105",
+    "url": "/static/js/runtime-main.9ac5517d.js"
+  }
+]);
