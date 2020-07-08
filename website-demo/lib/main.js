92abf14d3f40685cdd9bcb93bd78100670c41adb
Akshay Khale <khaleakshay@gmail.com>
2020-07-09 01:40:45 +0530

    Added personal website demo for testing

---
+++ website-demo/lib/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Hello Bulma!');
+});
+
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
