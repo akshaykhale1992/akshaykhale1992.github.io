0c669cc4acd5bd0a4db9a406ce1bf82c2bcc0350
Akshay Khale <khaleakshay@gmail.com>
2020-07-11 22:12:57 +0530

    Updated Quotes API

---
+++ quotes/precache-manifest.e41d74aaee0f0f89a3b4fbdfbbab9bb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452dfe40be9c41559f5f8b49c09ef3ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5012dcf564ec5aa1015b",
+    "url": "/static/css/main.1e15c5f8.chunk.css"
+  },
+  {
+    "revision": "d2fc0fe64fb0fc75477e",
+    "url": "/static/js/2.6768f762.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6768f762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5012dcf564ec5aa1015b",
+    "url": "/static/js/main.943ad2c4.chunk.js"
+  },
+  {
+    "revision": "573c1ef5acecc4a28105",
+    "url": "/static/js/runtime-main.9ac5517d.js"
+  }
+]);
