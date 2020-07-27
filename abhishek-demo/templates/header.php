0fdeef9a2ceee3ccfa028d50ea9e54ec490a7291
Akshay Khale <khaleakshay@gmail.com>
2020-07-27 16:20:25 +0530

    removed video

--- abhishek-demo/templates/header.php
+++ abhishek-demo/templates/header.php
@@ -7,12 +7,6 @@
                   </h1>
               </div>
               <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-                  <div class="row" style="margin-bottom: 30px;">
-                      <div class="col-md-12">
-                          <iframe src="https://www.youtube.com/embed/-xPH5Z27CyQ" style="width:100%;height:300px;">
-                          </iframe>
-                      </div>
-                  </div>
                   <form>
                       <div class="form-row">
                           <button type="submit" class="btn btn-block btn-lg btn-danger">
